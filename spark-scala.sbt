9ad697a8c2326c3505eb8251fc99e4b9b57fc259
Isaac Peña <isaac@repson.org>
2017-12-13 22:59:45 +0100

    Added SparkStreamingKafka.scala driver

---
+++ spark-scala.sbt
@@ -0,0 +1,7 @@
+name := "Spark Scala"
+
+version := "1.0"
+
+scalaVersion := "2.12.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % ""
---
+++ src/main/scala/SparkStreamingKafka.scala
@@ -0,0 +1,45 @@
+import _root_.kafka.serializer.DefaultDecoder
+import _root_.kafka.serializer.StringDecoder
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming._
+import org.apache.log4j.Logger
+
+object SimpleStreaming { 
+
+    val localLogger = Logger.getLogger("SimpleStreaming")
+
+    def main(args: Array[String]){
+        val sc = new SparkConf().setAppName("SimpleStreamingApp")
+ 
+        // creating the StreamingContext with 5 seconds interval
+        val ssc = new StreamingContext(sc, Seconds(5))
+
+        val kafkaTopicRaw = "test"
+        val kafkaBroker = "localhost:9092"
+        val topics: Set[String] = kafkaTopicRaw.split(",").map(_.trim).toSet
+
+        val kafkaConf = Map(
+            "metadata.broker.list" -> kafkaBroker,
+            "zookeeper.connect" -> "localhost:2181",
+            "group.id" -> "kafka-streaming-example",
+            "zookeeper.connection.timeout.ms" -> "1000"
+        )
+ 
+        val kafkaParams = Map[String, String](
+            "metadata.broker.list" -> kafkaBroker
+        )
+
+        val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+            ssc, 
+            kafkaParams, 
+            topics
+        )
+ 
+        val words = stream.flatMap{case(x, y) => y.split(" ")}
+ 
+        words.print()
+ 
+        ssc.start()
+    }
+}
